30badebe4b68a56d583191b814b1a210ea2e7d95
Tanvi Goyal <tgoyal942@gmail.com>
2026-02-18 13:00:04 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator{
+      public static void main(String[] args){
+      int a=5;
+      int b=7;
+      int c=a+b;
+      System.out.println("sum is "+ c);
+      }
+}      
+             
